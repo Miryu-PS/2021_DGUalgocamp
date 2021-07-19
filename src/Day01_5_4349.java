86f02649fcccbb33b957e5e0edfdd753155a271b
Miryu-PS <ms990606@naver.com>
2021-07-20 04:35:48 +0900

    add
    ,

---
+++ src/Day01_5_4349.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Day01_5_4349 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		int[][] arr = new int[105][2];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			arr[i][0] = Integer.parseInt(st.nextToken());
+			arr[i][1] = Integer.parseInt(st.nextToken());
+		}
+		int Maxarea = 0;
+		for (int i = 0; i < N; i++) { // 중심점 i
+			for (int j = 0; j < N; j++) { // i와 x좌표가 같은 j
+				for (int k = 0; k < N; k++) { // i와 y좌표가 같은 k
+					if (i == j || j == k || k == i) // 점을 중복해서 골랐을 경우
+						continue;
+					if (arr[i][0] == arr[j][0] && arr[i][1] == arr[k][1]) {
+						int width = arr[i][0] - arr[k][0];
+						int height = arr[i][1] - arr[j][1];
+						if (width < 0)
+							width = -width;
+						if (height < 0)
+							height = -height;
+						int area = width * height;
+						if (Maxarea < area)
+							Maxarea = area;
+					}
+				}
+			}
+		}
+		System.out.println(Maxarea);
+	}
+
+}
---
+++ src/Day01_6_2941.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Day01_6_2941 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int count = 0;
+		for (int i = 1; i <= N; i++) {
+			for (int j = i; j <= N; j++) {
+				for (int k = j; k <= N; k++) {
+					if (i + j + k == N && k < i + j)
+						count++;
+				}
+			}
+		}
+		
+		System.out.println(count);
+	}
+
+	public static void main_(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		int count = 0;
+		for (int i = 1; i <= N; i++) {
+			for (int j = i; j <= N; j++) {
+				int k = N - i - j;
+				if (j <= k && k < i + j)
+					count++;
+			}
+		}
+		
+		System.out.println(count);
+	}
+}
