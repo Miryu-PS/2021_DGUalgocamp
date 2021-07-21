aca6b1c1e790c2f9e9b5dcff3b8fabb4e58acf5c
Miryu-PS <ms990606@naver.com>
2021-07-21 10:38:17 +0900

    update
    .

--- 1일차/Day01_울타리색칠하기.java
+++ 1일차/Day01_울타리색칠하기.java
@@ -1,25 +1,33 @@
-import java.util.*;
 
-public class main {
-	
-	
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		
-		int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt(), d = sc.nextInt();
-		
-		int l1 = b - a, l2 = d - c, l = 0, x1, x2;
-		
-		if (c < b) {
-			if (a < c) x1 = c;
-			else x1 = a;
-			
-			if (b < d) x2 = b;
-			else x2 = d;
-			
-			l = x2 - x1;
-		}
-		
-		System.out.println(l1 + l2 - l);
-	}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+public class main{
+    public static void main(String[] args) throws IOException{ // BufferedReader를 사용하기 위해서는 해당 메소드에 throws IOException 구문이 필수입니다.
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken()), d = Integer.parseInt(st.nextToken());
+        
+        boolean[] x = new boolean[100]; // x[i]; [i, i + 1)인 구간에 색칠되어있는지 여부
+        
+        for(int i = 0; i < 100; i++) x[i] = false;
+        for(int i = a; i < b; i++) {
+        	x[i] = true;
+        }
+        for(int i = c; i < d; i++) {
+        	x[i] = true;
+        }
+        
+        int ans = 0;
+        for(int i = 0; i < 100; i++) {
+        	if (x[i]) ans++;
+        }
+        System.out.println(ans);
+    }
 }
